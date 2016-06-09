65b60dd0cec1b06b28f45fe0dc5101914b464e6b
Seungho Yang <Seungho Yang>
2016-06-09 17:36:31 +0900

    main에서 PrintInterface 함수 추가, 기본 swtich-case 틀 생성

---
+++ DS_Project(feat.Git)/DS_Project(feat.Git)/Main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#define TRUE	1
+#define FALSE	0
+
+void PrintInterface();
+
+void PrintInterface()
+{
+	printf("0. Read data files\n");
+	printf("1. Display statistics\n");
+	printf("2. Top 5 most tweeted words\n");
+	printf("3. Top 5 most tweeted users\n");
+	printf("4. Find users who tweeted a word\n");
+	printf("5. Find all people who are friends of the above users\n");
+	printf("6. Delete all mentions of a word\n");
+	printf("7. Delete all users who mentioned a word\n");
+	printf("8. Find strongly connected components\n");
+	printf("9. Find shortest path from a given user\n");
+	printf("99. Quit\n");
+	printf("Select Menu: ");
+}
+
+int main(void)
+{
+	int selectMenu;
+
+	while (TRUE)
+	{
+		PrintInterface();
+		scanf("%d", &selectMenu);
+		gets();
+
+		switch (selectMenu)
+		{
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		default:
+			break;
+		}
+	}
+}
